926edf269ec47a107eec06f2f1e7610d2969d7af
v02qabs <takesue090@gmail.com>
2020-02-24 12:43:02 +0900

    Addproject-for-AIDE-MyKakeibo

---
+++ MyKakeiboAIDE/app/src/main/java/my/kakeibo/aide/MainActivity.java
@@ -0,0 +1,14 @@
+package my.kakeibo.aide;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
