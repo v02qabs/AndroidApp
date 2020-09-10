5ca374325642ea10ca4582b9b92504cbd89b49bc
v02qabs <takesue090@gmail.com>
2020-09-10 18:10:49 +0900

    add context unable phone.

--- MyNote/app/src/main/java/com/mycompany/mynote/MainActivity.java
+++ MyNote/app/src/main/java/com/mycompany/mynote/MainActivity.java
@@ -2,6 +2,9 @@ package com.mycompany.mynote;
 
 import android.app.*;
 import android.os.*;
+import org.apache.http.client.utils.*;
+import android.content.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -9,6 +12,29 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+		makeing_thread();
         setContentView(R.layout.main);
-    }
+	}
+	private void makeing_thread()
+	{
+		Runnable mr = new MyRunnable(this);
+		new Thread(mr).start();
+		
+	}
+	
 }
+
+class MyRunnable implements Runnable
+{
+	Context co;
+	public MyRunnable(Context context)
+	{
+		this.co = context;
+	}
+	@Override
+	public void run()
+	{
+		Toast.makeText(co, "Hello", Toast.LENGTH_LONG).show();
+	}
+}
+
