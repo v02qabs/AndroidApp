83b251d9010b4850eba4dca6b432fdc792cd3215
v02qabs <takesue090@gmail.com>
2020-09-11 17:30:21 +0900

    Theme.blak.test.1

--- MyNote/app/src/main/java/com/mycompany/mynote/MainActivity.java
+++ MyNote/app/src/main/java/com/mycompany/mynote/MainActivity.java
@@ -5,36 +5,27 @@ import android.os.*;
 import org.apache.http.client.utils.*;
 import android.content.*;
 import android.widget.*;
-
+import java.lang.*;
+import android.support.v7.app.*;
 public class MainActivity extends Activity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		makeing_thread();
-        setContentView(R.layout.main);
+		try
+		{
+			makeing_thread();
+        }catch(Exception error)
+		{
+			
+		}//setContentView(R.layout.main);
 	}
 	private void makeing_thread()
 	{
-		Runnable mr = new MyRunnable(this);
-		new Thread(mr).start();
-		
+		MyRunnable mr = new MyRunnable(getApplicationContext());
+		mr.start();
 	}
 	
-}
-
-class MyRunnable implements Runnable
-{
-	Context co;
-	public MyRunnable(Context context)
-	{
-		this.co = context;
-	}
-	@Override
-	public void run()
-	{
-		Toast.makeText(co, "Hello", Toast.LENGTH_LONG).show();
-	}
 }
 
