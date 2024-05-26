205aadc1a5c0ac1b96c00e93da80a8bce5efdd9c
v02qabs <takesue090@gmail.com>
2024-05-26 21:04:03 +0900

    MyWebView-base

--- MyWebView/app/src/main/java/com/hiro/mywebview/MainActivity.java
+++ MyWebView/app/src/main/java/com/hiro/mywebview/MainActivity.java
@@ -1,14 +1,37 @@
 package com.hiro.mywebview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_CODE = 1;
+
+       private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());  // 画面遷移を WebView 内で処理する
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);  // JavaScript を有効にする
+
+        webView.loadUrl("https://www.google.co.jp");
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();  // WebView でバック可能ならバックする
+        } else {
+            super.onBackPressed();
+        }
     }
 }
+
