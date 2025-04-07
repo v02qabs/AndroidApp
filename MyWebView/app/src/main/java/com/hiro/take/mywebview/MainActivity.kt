1616504ade12f092c4a2d03cd60ce27f3c96bb68
v02qabs <takesue090@gmail.com>
2025-04-08 02:54:48 +0900

    show

--- MyWebView/app/src/main/java/com/hiro/take/mywebview/MainActivity.kt
+++ MyWebView/app/src/main/java/com/hiro/take/mywebview/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hiro.take.mywebview
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -21,15 +22,18 @@ class MainActivity : Activity() {
     private lateinit var urlEditText: EditText
     private lateinit var downloadLocalText: EditText
     private lateinit var downloadButton: Button
+    private lateinit var dir_command1: Button
     
+    private lateinit var from_size : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        
+        from_size = findViewById(R.id.from_site)
         urlEditText = findViewById(R.id.urlEditText)
         downloadLocalText = findViewById(R.id.download_local_Text)
         downloadButton = findViewById(R.id.downloadButton)
-
+					dir_command1 = findViewById(R.id.dir_command)
+					
         downloadButton.setOnClickListener {
             val fileUrl = urlEditText.text.toString()
             val fileName = downloadLocalText.text.toString()
@@ -38,8 +42,22 @@ class MainActivity : Activity() {
             } else {
                 Toast.makeText(this, "URLまたはファイル名を入力してください", Toast.LENGTH_SHORT).show()
             }
-        }
+            dir_command1.setOnClickListener({
+					show()
+					})
+        from_size.setOnClickListener({
+                // 起動する対象をクラスオブジェクトで指定する
+                val intent = Intent(this, Web::class.java)
+                //intent.putExtra("VALUE", editText.text.toString())
+                startActivity(intent)
+            }
+        );
     }
+   }
+   private fun show(){
+			Toast.makeText(this, "FileList: " , Toast.LENGTH_SHORT).show()
+		}
+   
 
     private fun downloadFile(fileUrl: String, fileName: String) {
         GlobalScope.launch(Dispatchers.Main) {
@@ -60,11 +78,12 @@ class MainActivity : Activity() {
 
             if (connection.responseCode != HttpURLConnection.HTTP_OK) {
                 return "サーバーエラー: ${connection.responseCode} ${connection.responseMessage}"
-            }
+								}
 
+            
             inputStream = BufferedInputStream(connection.inputStream)
             
-            // アプリ内の内部ストレージへの保存 (外部ストレージを使用しない)
+								// アプリ内の内部ストレージへの保存 (外部ストレージを使用しない)
             val outputFile = File(getExternalFilesDir(null), fileName)
             fileOutputStream = FileOutputStream(outputFile)
 
@@ -72,20 +91,20 @@ class MainActivity : Activity() {
             var bufferLength: Int
             while (inputStream.read(buffer).also { bufferLength = it } > 0) {
                 fileOutputStream.write(buffer, 0, bufferLength)
-            }
+								}
 
-            "ダウンロード完了: ${outputFile.absolutePath}"
+							"ダウンロード完了: ${outputFile.absolutePath}"
 
-        } catch (e: IOException) {
-            "エラー: ${e.message}"
-        } finally {
+						} catch (e: IOException) {
+								"エラー: ${e.message}"
+						} finally {
             try {
                 inputStream?.close()
                 fileOutputStream?.close()
             } catch (e: IOException) {
                 e.printStackTrace()
-            }
-        }
-    }
+						}
+					}	
+			}
 }
 
